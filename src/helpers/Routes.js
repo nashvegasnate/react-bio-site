e86361915e135a12ac6add8259fc4e0d44778a17
Nathan Keith <nashvegasnate@gmail.com>
2021-05-24 19:45:34 -0500

    added routes

---
+++ src/helpers/Routes.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import {
+  Switch,
+  Route,
+  Redirect
+} from 'react-router-dom';
+import PropTypes from 'prop-types';
+// import AddPlayer from '../views/AddPlayer';
+import Home from '../views/Home';
+import Projects from '../views/Projects';
+import About from '../views/About';
+import Tech from '../views/Tech';
+import Contact from '../views/Contact';
+import ProjectsCRUD from '../views/admin/ProjectsCRUD';
+
+
+
+// import Players from '../views/Players';
+// import SinglePlayer from '../views/SinglePlayer';
+// import NotFound from '../views/NotFound';
+
+const AuthedRoute = ({ component: Component, admin, ...rest }) => {
+  const routeChecker = (burrito) => (admin
+    ? (<Component {...burrito} admin={admin} />)
+    : (<Redirect to={{ pathname: '/', state: { from: burrito.location } }} />));
+  
+  return <Route {...rest} render={(props) => routeChecker(props)} />;
+};
+
+AuthedRoute.propTypes = {
+  component: PropTypes.func,
+  admin: PropTypes.any
+};
+function Routes({ admin }) {
+  return (
+    <div>
+      <Switch>
+        <Route
+          exact
+          path='/'
+          component={Home} />
+        <Route
+          exact
+          path='/about'
+          component={About}
+        />
+        <Route
+          exact
+          path='/projects'
+          component={Projects}
+        />
+        <Route
+          exact
+          path='/tech'
+          component={Tech}
+        />
+         <Route
+          exact
+          path='/contact'
+          component={Contact}
+        />
+        <AuthedRoute
+          exact
+          path='/projectsCrud'
+          admin={admin}
+          component={ProjectsCRUD}
+        />  
+        <Route path='*' component={Home} />
+      </Switch>
+    </div>
+  );
+}
+
+Routes.propTypes = {
+  admin: PropTypes.any
+};
+
+export default Routes;
