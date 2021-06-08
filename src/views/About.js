6f51b929cfb1fce2d2f05684ce0ea518d56007be
Nathan Keith <nashvegasnate@gmail.com>
2021-06-08 15:02:11 -0500

    deleted one space

--- src/views/About.js
+++ src/views/About.js
@@ -11,7 +11,7 @@ export default function About() {
       <div className='aboutTitle'>
         <h2>Who I Am</h2>
         <p>
-          I was born and raised in Nashville, and like many of my fellow Music  City natives, I have been playing music almost my entire life. I love music and have dedicated a lot of my time to mastering the bass guitar, however, I never pursued a career in music. Instead, I traveled down a different path toward film and video production, but after realizing the importance of steady, stable work, I decided to go to law school.
+          I was born and raised in Nashville, and like many of my fellow Music City natives, I have been playing music almost my entire life. I love music and have dedicated a lot of my time to mastering the bass guitar, however, I never pursued a career in music. Instead, I traveled down a different path toward film and video production, but after realizing the importance of steady, stable work, I decided to go to law school.
         </p>
         <p>
           After spending several years as an attorney, I felt that the creative flame that burned within me had died down and needed to be rekindled. Then, I discovered the world of web development, which seemed to demonstrate a delicate balance between structure and creativity that piqued my interest. I jumped into the part-time full-stack web development program at Nashville Software School and never looked back.
